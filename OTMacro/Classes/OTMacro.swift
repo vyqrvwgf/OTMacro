044638880f3523d80bcee794b4787d232f478c27
hanlei <hanlei@meiqia.com>
2018-04-10 17:47:46 +0800

    add: Regex

--- OTMacro/Classes/OTMacro.swift
+++ OTMacro/Classes/OTMacro.swift
@@ -31,3 +31,12 @@ public enum OTRegisterType: Int {
     case mail = 0
     case mobile = 1
 }
+
+public enum OTRegexExpression: String {
+    case mobile = "^1[3|4|5|7|8]\\d{9}"
+    case email = "[A-Z0-9a-z\\._%+-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}"
+    case phone = "^(\\d{3,4}-?)\\d{7,8}$"
+    case link = "((http[s]{0,1}|ftp)://[a-zA-Z0-9\\.\\-]+\\.([a-zA-Z]{2,4})(:\\d+)?(/[a-zA-Z0-9\\.\\-~!@#$%^&*+?:_/=<>]*)?)|(www.[a-zA-Z0-9\\.\\-]+\\.([a-zA-Z]{2,4})(:\\d+)?(/[a-zA-Z0-9\\.\\-~!@#$%^&*+?:_/=<>]*)?)"
+    case qq = "[1-9]\\d{4,10}"
+    case password = "^(?![0-9]+$)(?![A-Za-z]+$)[0-9a-zA-Z!@#$%^&*_`()-+=]{8,20}$"
+}
